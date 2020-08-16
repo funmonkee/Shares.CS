77605d79de5776c7886f020fcf872420bfe86cd0
“paul <pblack9455@yahoo.com>
2020-08-16 12:09:12 +0100

    init repos and add empty solution

---
+++ Shares.ConsoleApp.UnitTest/ConsoleApplicationTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Shares.ConsoleApp.UnitTest
+{
+    public class ConsoleApplicationTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Shares.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace Shares.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
